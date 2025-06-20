13228a0e8f47d579476d9c5a68d3f2a81a8b59a3
juanfsouza <juanfsouza@gmail.com>
2025-06-20 05:14:53 -0300

    fix: data

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,14 +4,19 @@ dotenv.config();
 
 module.exports = new DataSource({
   type: 'postgres',
-  url: process.env.DIRECT_URL,
-  host: process.env.DB_HOST || 'aws-0-sa-east-1.pooler.supabase.com',
+  host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USERNAME || 'postgres.drrmcieepvakoepjqfax',
-  password: process.env.DB_PASSWORD || 'qwaszxc123@',
-  database: process.env.DB_NAME || 'postgres',
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME || 'conectar',
   synchronize: false,
   logging: true,
-  entities: [require('./domain/entities/user.entity')],
-  migrations: ['src/migrations/*.ts'],
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+  entities: [__dirname + '/domain/entities/*.entity.{js,ts}'],
+  migrations: [__dirname + '/migrations/*.{js,ts}'],
 });
