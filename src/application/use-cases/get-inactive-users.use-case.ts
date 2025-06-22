6e57a2d6866dbcc4278b1f89fa45126a4cfb520a
juanfsouza <juanfsouza@gmail.com>
2025-06-22 16:20:06 -0300

    fix: case get users inactive

--- src/application/use-cases/get-inactive-users.use-case.ts
+++ src/application/use-cases/get-inactive-users.use-case.ts
@@ -1,15 +1,24 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { IUserRepository } from '../../domain/interfaces/user.repository.interface';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { User } from '../../domain/entities/user.entity';
 
 @Injectable()
 export class GetInactiveUsersUseCase {
   constructor(
-    @Inject('IUserRepository')
-    private readonly userRepository: IUserRepository,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
   ) {}
 
-  async execute(days: number): Promise<User[]> {
-    return this.userRepository.findInactiveUsers(days);
+  async execute(daysThreshold: number = 30): Promise<User[]> {
+    const thresholdDate = new Date();
+    thresholdDate.setDate(thresholdDate.getDate() - daysThreshold);
+
+    const inactiveUsers = await this.userRepository
+      .createQueryBuilder('user')
+      .where('user.lastLogin IS NULL OR user.lastLogin < :threshold', { threshold: thresholdDate })
+      .getMany();
+
+    return inactiveUsers;
   }
 }
